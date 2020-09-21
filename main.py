f82ccff74c1bd6a508d87f4b9caca92633916db7
Harry Chan <taiharry108@gmail.com>
2020-09-21 14:12:33 -0700

    add fav thum pig

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import main, google_auth, user
-from core.downloader import Downloader
 from fastapi.logger import logger
 from core.singleton_aiohttp import SingletonAiohttp
 from database import models
 from database.database import engine
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -17,7 +18,7 @@ origins = ["http://localhost:4200"]
 
 async def on_start_up():
     fastAPI_logger.info("on_start_up")
-    # d = Downloader(SingletonAiohttp.get_session())    
+    # d = Downloader(SingletonAiohttp.get_session())
 
 
 async def on_shutdown():
@@ -25,6 +26,7 @@ async def on_shutdown():
     await SingletonAiohttp.close_aiohttp_client()
 
 app = FastAPI(on_startup=[on_start_up], on_shutdown=[on_shutdown])
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
